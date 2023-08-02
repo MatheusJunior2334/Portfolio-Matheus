75bd75e0125410eb780eeaa6a5cb5c60780c3bae
MatheusJunior2334 <matheusjuniormjg2334@gmail.com>
2023-08-02 02:06:10 -0300

    feat: Novas adições e melhorias no código

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="pt">
       <head>
-        <link rel="shortcut icon" href="next.svg" type="image/x-icon" />
+        <link rel="icon" href="next.svg" type="image/x-icon" />
       </head>
 
       <body className={readexPro.className}>{children}</body>
