279818efcb3276cb8bda88d9f510278ea8e1fa30
MatheusJunior2334 <matheusjuniormjg2334@gmail.com>
2024-07-15 02:30:30 -0300

    fix: fixing deploy error (try 1).

--- public/assets/images/projects/projectImages.tsx
+++ public/assets/images/projects/projectImages.tsx
@@ -4,7 +4,7 @@ import MarcioImg from './MarcioMotoristaEspecialImage.webp';
 import JMBankImg from './JMBankImage.webp';
 import PectoImg from './PectoImage.webp';
 import FireEmblemImg from './FireEmblemAwakeningConversationsImage.webp';
-import LonyImg from './LonyLadiesofNewYorkImage.webp';
+import LonyImg from './LonyLadiesOfNewYorkImage.webp';
 import YoutubeImg from './YoutubeCloneImage.webp';
 
 export {NostalgicWorldImg, HBankImg, MarcioImg, JMBankImg, PectoImg, YoutubeImg, FireEmblemImg, LonyImg}
